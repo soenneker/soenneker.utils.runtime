5b3bfe49fec3b70ca79ba3c6911bbc8ba701b5a5
Jake Soenneker <jake@soenneker.com>
2024-07-07 10:11:07 -0500

    misc perf

--- src/RuntimeUtil.cs
+++ src/RuntimeUtil.cs
@@ -1,37 +1,68 @@
 using System;
+using System.Diagnostics.Contracts;
 
 namespace Soenneker.Utils.Runtime;
 
 /// <summary>
 /// A collection of helpful runtime-based operations
 /// </summary>
-public class RuntimeUtil
+public static class RuntimeUtil
 {
+    /// <summary>
+    /// Determines whether the current operating system is Windows.
+    /// </summary>
+    /// <returns>true if the current operating system is Windows; otherwise, false.</returns>
+    [Pure]
     public static bool IsWindows()
     {
         return OperatingSystem.IsWindows();
     }
 
+    /// <summary>
+    /// Determines whether the current operating system is macOS.
+    /// </summary>
+    /// <returns>true if the current operating system is macOS; otherwise, false.</returns>
+    [Pure]
     public static bool IsMacOs()
     {
         return OperatingSystem.IsMacOS();
     }
 
+    /// <summary>
+    /// Determines whether the current operating system is Linux.
+    /// </summary>
+    /// <returns>true if the current operating system is Linux; otherwise, false.</returns>
+    [Pure]
     public static bool IsLinux()
     {
         return OperatingSystem.IsLinux();
     }
 
+    /// <summary>
+    /// Determines whether the current operating system is Android.
+    /// </summary>
+    /// <returns>true if the current operating system is Android; otherwise, false.</returns>
+    [Pure]
     public static bool IsAndroid()
     {
         return OperatingSystem.IsAndroid();
     }
 
+    /// <summary>
+    /// Determines whether the current operating system is a browser.
+    /// </summary>
+    /// <returns>true if the current operating system is a browser; otherwise, false.</returns>
+    [Pure]
     public static bool IsBrowser()
     {
         return OperatingSystem.IsBrowser();
     }
 
+    /// <summary>
+    /// Determines whether the current operating system is iOS.
+    /// </summary>
+    /// <returns>true if the current operating system is iOS; otherwise, false.</returns>
+    [Pure]
     public static bool IsIos()
     {
         return OperatingSystem.IsIOS();
